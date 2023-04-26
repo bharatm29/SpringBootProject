4cc82202155a68e8f8c14926b5eefd2f2ee65e57
Bharat <bharatmhes@gmail.com>
2023-04-26 10:35:16 +0530

    Improving docs

--- src/main/java/io/bharat/mvc/GameController.java
+++ src/main/java/io/bharat/mvc/GameController.java
@@ -5,11 +5,15 @@ import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.bharat.model.Game;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
 @RequestMapping("/games")
@@ -18,7 +22,7 @@ public class GameController {
 	@Autowired
 	private GameRepository gameRepository;
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	public List<Game> getAllGames(){
 		List<Game> games = new ArrayList<>();
 		List<Game> buff= new ArrayList<>();
@@ -29,14 +33,20 @@ public class GameController {
 		return games;
 	}
 	
-	@RequestMapping("/{title}")
+	@Operation(summary = "Get all games matching the title", 
+				responses = {
+						@ApiResponse(responseCode = "200", description = "Found all the titles",
+								content = @Content(mediaType = "application/json")),
+						@ApiResponse(responseCode = "404", description = "Could not found the titles")
+				})
+	@GetMapping("/{title}")
 	public List<Game> getGame(@PathVariable String title) {
 		List<Game> games = new ArrayList<>();
 		gameRepository.findByTitleContainingIgnoreCase(title).forEach(games::add);
 		return games.stream().sorted(Comparator.comparing(Game::getRank)).toList();
 	}
 	
-	@RequestMapping("/rating/{rating}")
+	@GetMapping("/rating/{rating}")
 	public List<Game> getByRating(@PathVariable String rating){
 		List<Game> games = new ArrayList<>();
 
