0df3bbb922646f3369da47f37e0caf25a49c5ac9
Bharat <bharatmhes@gmail.com>
2023-05-03 08:00:22 +0530

    Added Searching Functionality in movie-info-service with ID

---
+++ movie-info/src/main/java/com/bharat/movieinfo/models/MovieLists.java
@@ -0,0 +1,22 @@
+package com.bharat.movieinfo.models;
+
+import java.util.List;
+
+public class MovieLists {
+    private List<MovieSummary> results;
+
+    public MovieLists() {
+    }
+
+    public MovieLists(List<MovieSummary> results) {
+        this.results = results;
+    }
+
+    public List<MovieSummary> getResults() {
+        return results;
+    }
+
+    public void setResults(List<MovieSummary> results) {
+        this.results = results;
+    }
+}
