1b1f80ec47ab928b4535f3be9fc0610346e1a895
Bharat <bharatmhes@gmail.com>
2023-05-12 13:44:00 +0530

    Added circuitBreaker to apod enpoint

--- src/main/java/com/space/SpaceAPI/SpaceApiApplication.java
+++ src/main/java/com/space/SpaceAPI/SpaceApiApplication.java
@@ -2,6 +2,8 @@ package com.space.SpaceAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpaceApiApplication {
@@ -10,4 +12,8 @@ public class SpaceApiApplication {
 		SpringApplication.run(SpaceApiApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
 }
--- src/main/java/com/space/SpaceAPI/resource/SpaceController.java
+++ src/main/java/com/space/SpaceAPI/resource/SpaceController.java
@@ -1,14 +1,33 @@
 package com.space.SpaceAPI.resource;
 
 import com.space.SpaceAPI.models.AstroPicture;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/space")
 public class SpaceController {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${apod.url}")
+    private String apodUrl;
+
+    @Value("${api.key}")
+    private String apiKey;
+
     @GetMapping("/apod/{date}")
+    @CircuitBreaker(name = "apodCB", fallbackMethod = "apodFallBack")
     public @ResponseBody AstroPicture getAPOD(@PathVariable String date){
-        
+        return restTemplate.getForObject(apodUrl + apiKey + "&date=" + date,
+                                        AstroPicture.class);
+    }
+
+    public AstroPicture apodFallBack(String date, Exception e){
+        return new AstroPicture("No image found", "null", date, "Server failed to retrieve the image");
     }
 }
