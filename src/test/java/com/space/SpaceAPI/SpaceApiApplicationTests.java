30dde0ebb8217244f787fd1323e532f919e23efe
Bharat <bharatmhes@gmail.com>
2023-05-12 13:20:22 +0530

    Starting the SpaceAPI project

---
+++ src/test/java/com/space/SpaceAPI/SpaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.space.SpaceAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
