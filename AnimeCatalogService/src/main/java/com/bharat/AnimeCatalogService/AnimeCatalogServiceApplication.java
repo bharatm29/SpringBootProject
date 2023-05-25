8a147d170e72e1870018dbcb323153103b515bf4
Bharat <bharatmhes@gmail.com>
2023-05-25 21:25:16 +0530

    Initialized the project

---
+++ AnimeCatalogService/src/main/java/com/bharat/AnimeCatalogService/AnimeCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bharat.AnimeCatalogService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ UserCatalogService/src/main/java/com/bharat/UserCatalogService/UserCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bharat.UserCatalogService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCatalogServiceApplication.class, args);
+	}
+
+}
