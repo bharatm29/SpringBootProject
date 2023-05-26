578ab93d053ca97c577a0aa293aecf0bfe58f279
Bharat <bharatmhes@gmail.com>
2023-05-26 16:44:44 +0530

    Cleaning up some functionality

--- AnimeCatalogService/src/main/java/com/bharat/AnimeCatalogService/services/AnimeService.java
+++ AnimeCatalogService/src/main/java/com/bharat/AnimeCatalogService/services/AnimeService.java
@@ -1,12 +1,12 @@
 package com.bharat.AnimeCatalogService.services;
 
-import com.bharat.AnimeCatalogService.models.animesModels.AnimeDetails;
-import com.bharat.AnimeCatalogService.models.animesModels.AnimeGenresSearch;
-import com.bharat.AnimeCatalogService.models.animesModels.AnimeSearch;
-import com.bharat.AnimeCatalogService.models.animesModels.AnimeTopAiringSearch;
+import com.bharat.AnimeCatalogService.models.animesModels.*;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 public class AnimeService {
@@ -15,19 +15,45 @@ public class AnimeService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @CircuitBreaker(name = "search-cb", fallbackMethod = "searchFallback")
     public AnimeSearch searchAnime(String name){
         return restTemplate.getForObject(ANIMEINFOAPI_URI + "/search/" + name, AnimeSearch.class);
     }
 
+    @CircuitBreaker(name = "details-cb", fallbackMethod = "detailsFallBack")
     public AnimeDetails getAnimeDetails(String animeid){
         return restTemplate.getForObject(ANIMEINFOAPI_URI + "/details/" + animeid, AnimeDetails.class);
     }
 
+    @CircuitBreaker(name = "topAiring-cb", fallbackMethod = "topAiringFallback")
     public AnimeTopAiringSearch getTopAiringAnimes(){
         return restTemplate.getForObject(ANIMEINFOAPI_URI + "/top-airing", AnimeTopAiringSearch.class);
     }
 
+    @CircuitBreaker(name = "genre-cb", fallbackMethod = "genresSearchFallBack")
     public AnimeGenresSearch getAnimesOfGenres(String genres){
         return restTemplate.getForObject(ANIMEINFOAPI_URI + "/genre/" + genres, AnimeGenresSearch.class);
+    }
+
+    public AnimeSearch searchFallback(String name, Exception e){
+        return AnimeSearch.builder().animes(List.of(
+                Anime.builder().animeTitle("Could not found").build()
+        )).build();
+    }
+
+    public AnimeDetails detailsFallBack(String animeId, Exception e){
+        return AnimeDetails.builder().animeTitle("Could not found this anime details").build();
+    }
+
+    public AnimeTopAiringSearch topAiringFallback(Exception e){
+        return AnimeTopAiringSearch.builder().animes(
+                List.of(AnimeTopAiring.builder().animeTitle("Could not find any animes").build())
+        ).build();
+    }
+
+    public AnimeGenresSearch genresSearchFallBack(String genres, Exception e){
+        return AnimeGenresSearch.builder().animes(
+                List.of(AnimeGenres.builder().animeTitle("Could not find any anime of this genre").build())
+        ).build();
     }
 }
--- AnimeCatalogService/src/main/java/com/bharat/AnimeCatalogService/services/AnimeUserService.java
+++ AnimeCatalogService/src/main/java/com/bharat/AnimeCatalogService/services/AnimeUserService.java
@@ -4,6 +4,7 @@ import com.bharat.AnimeCatalogService.models.animesModels.AnimeDetails;
 import com.bharat.AnimeCatalogService.models.AnimeResponse;
 import com.bharat.AnimeCatalogService.models.AnimeUser;
 import com.bharat.AnimeCatalogService.models.AnimeUserResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,11 +17,13 @@ public class AnimeUserService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @CircuitBreaker(name = "post-cb", fallbackMethod = "postFallback")
     public AnimeResponse addAnimeUser(AnimeUser animeUser){
         AnimeResponse animeResponse = restTemplate.postForObject(USER_URI + "/add", animeUser, AnimeResponse.class);
         return animeResponse;
     }
 
+    @CircuitBreaker(name = "get-cb", fallbackMethod = "getFallback")
     public AnimeUserResponse getAnimeUser(String email){
         AnimeUser animeUser = restTemplate.getForObject(USER_URI + "/" + email, AnimeUser.class);
         return AnimeUserResponse.builder()
@@ -31,7 +34,20 @@ public class AnimeUserService {
                 .build();
     }
 
+    @CircuitBreaker(name = "update-cb", fallbackMethod = "updateFallback")
     public AnimeResponse updateAnimeUser(String email, AnimeUser animeUser){
         return restTemplate.patchForObject(USER_URI + "/" + email, animeUser, AnimeResponse.class);
+    }
+
+    public AnimeUserResponse getFallback(String email, Exception e){
+        return AnimeUserResponse.builder().email("Could not resolve the user").build();
+    }
+
+    public AnimeResponse postFallback(AnimeUser animeUser, Exception e){
+        return AnimeResponse.builder().message("Could not add the user").build();
+    }
+
+    public AnimeResponse updateFallback(String email, Exception e){
+        return AnimeResponse.builder().message("Could not update the user").build();
     }
 }
