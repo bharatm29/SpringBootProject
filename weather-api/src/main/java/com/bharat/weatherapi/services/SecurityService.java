4cc32610a82a76b2c00fb3fee1a80d71f5c436bd
Bharat <bharatmhes@gmail.com>
2023-06-17 08:58:40 +0530

    Configured cors

---
+++ weather-api/src/main/java/com/bharat/weatherapi/services/SecurityService.java
@@ -0,0 +1,22 @@
+package com.bharat.weatherapi.services;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class SecurityService {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        UrlBasedCorsConfigurationSource corsConfig = new UrlBasedCorsConfigurationSource();
+        corsConfig.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        http.csrf().disable()
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfig))
+                .authorizeHttpRequests().anyRequest().permitAll();
+
+        return http.build();
+    }
+}
