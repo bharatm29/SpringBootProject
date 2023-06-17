7e6eea4f42f3eaf883b881841f69d0aabd779d0c
Bharat <bharatmhes@gmail.com>
2023-06-17 09:36:31 +0530

    Added Circuitbreakers for 429 and location not found

--- weather-api/src/main/java/com/bharat/weatherapi/services/WeatherService.java
+++ weather-api/src/main/java/com/bharat/weatherapi/services/WeatherService.java
@@ -1,16 +1,21 @@
 package com.bharat.weatherapi.services;
 
-import com.bharat.weatherapi.models.DateHandler;
-import com.bharat.weatherapi.models.WeatherCodes;
-import com.bharat.weatherapi.models.WeatherForecastDaily;
+import com.bharat.weatherapi.models.*;
+import com.bharat.weatherapi.models.realtimeForecast.RealtimeForecastData;
+import com.bharat.weatherapi.models.wrapper.dailyForecast.DailyForecast;
+import com.bharat.weatherapi.models.wrapper.dailyForecast.DailyForecastData;
 import com.bharat.weatherapi.models.wrapper.dailyForecast.WeatherForecastDailyWrapper;
-import com.bharat.weatherapi.models.WeatherForecastHourly;
 import com.bharat.weatherapi.models.realtimeForecast.RealtimeForecast;
+import com.bharat.weatherapi.models.wrapper.hourlyForecast.ForecastData;
+import com.bharat.weatherapi.models.wrapper.hourlyForecast.HourlyForecast;
 import com.bharat.weatherapi.models.wrapper.hourlyForecast.WeatherForecastHourlyWrapper;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 public class WeatherService {
@@ -35,6 +40,7 @@ public class WeatherService {
     @Autowired
     private WeatherCodes weatherCodes;
 
+    @CircuitBreaker(name = "realtime-cb", fallbackMethod = "getRealtimeFallback")
     public RealtimeForecast getRealtimeWeather(String location) {
         String url = realtimeUrl + "?location=" + location + "&apikey=" + apiKey;
 
@@ -49,6 +55,7 @@ public class WeatherService {
         return realtimeForecast;
     }
 
+    @CircuitBreaker(name = "hourly-cb", fallbackMethod = "getHourlyForecastFallback")
     public WeatherForecastHourly getWeatherForecastHourly(String location, Integer page) {
         String url = forecastUrl + "?location=" + location + "&apikey=" + apiKey + "&timesteps=1h";
 
@@ -56,11 +63,59 @@ public class WeatherService {
 
         return weatherForecastHourly.makeObjectFromWrapper(hourlyRes, page);
     }
+
+    @CircuitBreaker(name = "daily-cb", fallbackMethod = "getDailyForecastFallback")
     public WeatherForecastDaily getWeatherForecastDaily(String location) {
         String url = forecastUrl + "?location=" + location + "&apikey=" + apiKey + "&timesteps=1d";
 
         WeatherForecastDailyWrapper dailyRes = restTemplate.getForObject(url, WeatherForecastDailyWrapper.class);
 
         return weatherForecastDaily.makeObjectFromWrapper(dailyRes);
+    }
+
+    public RealtimeForecast getRealtimeFallback(String location, Exception e){
+        if(e.getMessage().contains("429")){
+            return RealtimeForecast.builder()
+                    .location(LocationData.builder().name("Too many requests").build())
+                    .data(RealtimeForecastData.builder()
+                            .time("No data found for the location")
+                            .values(ForecastData.builder().weatherCode("0").build())
+                            .build())
+                    .build();
+        }
+        return RealtimeForecast.builder()
+                .location(LocationData.builder().name("Location not found").build())
+                .data(RealtimeForecastData.builder().time("No data found for the location").build())
+                .build();
+    }
+    public WeatherForecastHourly getHourlyForecastFallback(String location, Integer page, Exception e){
+        if(e.getMessage().contains("429")){
+            return WeatherForecastHourly.builder()
+                    .location(LocationData.builder().name("Too many requests").build())
+                    .data(List.of(HourlyForecast.builder()
+                            .time("No data found for the location")
+                            .values(ForecastData.builder().weatherCode("0").build())
+                            .build()))
+                    .build();
+        }
+        return WeatherForecastHourly.builder()
+                .location(LocationData.builder().name("Location not found").build())
+                .data(List.of(HourlyForecast.builder().time("No data found for the location").build()))
+                .build();
+    }
+    public WeatherForecastDaily getDailyForecastFallback(String location, Exception e){
+        if(e.getMessage().contains("429")){
+            return WeatherForecastDaily.builder()
+                    .location(LocationData.builder().name("Too many requests").build())
+                    .data(List.of(DailyForecast.builder()
+                            .time("No data found for the location")
+                            .values(DailyForecastData.builder().weatherCodeMax("0").build())
+                            .build()))
+                    .build();
+        }
+        return WeatherForecastDaily.builder()
+                .location(LocationData.builder().name("Location not found").build())
+                .data(List.of(DailyForecast.builder().time("No data found for the location").build()))
+                .build();
     }
 }
