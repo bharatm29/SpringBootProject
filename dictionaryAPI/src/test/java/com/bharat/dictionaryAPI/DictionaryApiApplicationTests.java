a8613ac82ff40171baa4f110b0bec10c9e06b04b
Bharat <bharatmhes@gmail.com>
2023-06-28 19:49:45 +0530

    Initiated the DictionaryAPI

---
+++ dictionaryAPI/src/test/java/com/bharat/dictionaryAPI/DictionaryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharat.dictionaryAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
