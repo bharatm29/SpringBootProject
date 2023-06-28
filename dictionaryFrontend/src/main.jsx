44e80094e3355c313db93ded4a5381f2c104e36d
Bharat <bharatmhes@gmail.com>
2023-06-28 20:43:47 +0530

    Initialized React frontend

---
+++ dictionaryFrontend/src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const quertClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <QueryClientProvider client={quertClient}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </QueryClientProvider>
+    </React.StrictMode>
+);
